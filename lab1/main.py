8ed9702b13aa56a1cedb37a4f17caf5cecb2ed52
plmokn210 <61922986+plmokn210@users.noreply.github.com>
2023-02-23 20:31:35 +0200

    some comments

--- lab1/main.py
+++ lab1/main.py
@@ -1,32 +1,34 @@
-import sys
-from preToPostfix import preToPostfix
+#This file reads the input file from the command line, runs the preToPostfix function on each line of the input file, and writes the output to the output file from the command line.
 
-if __name__ == "__main__":
-    # Get the input and output file names from the command-line arguments
-    input_file = sys.argv[1]
-    output_file = sys.argv[2]
+import sys # Import the sys module, this is used to get the command-line arguments
+from preToPostfix import preToPostfix  # Import the preToPostfix function from the preToPostfix file
 
-    # Open the input file for reading
+if __name__ == "__main__": 
+    # Get the input and output file names from the command-line arguments. The first argument is the name of the input file, the second argument is the name of the output file
+    input_file = sys.argv[1] 
+    output_file = sys.argv[2] 
+
+    # Open and read the input file
     with open(input_file, "r") as file:
-        # Read the prefix expression from the input file
+        # Read the input file
         prefix_expression = file.read().strip()
 
-    # Convert the prefix expression to postfix
+    # Convert the prefix expressions to postfix by calling the preToPostfix function on the prefix expressions from the input file
     postfix_expression = preToPostfix(prefix_expression)
 
     # Open the output file for writing
     with open(input_file, "r") as input_file_obj, open(output_file, "w") as output_file_obj:
         # Read each line from the input file
         for line in input_file_obj:
-            # Strip whitespace from the line
+            # Removes whitespace from the line
             line = line.strip()
 
             # Skip empty lines
             if not line:
                 continue
 
-            # Convert the prefix expression to postfix
+            # Call the function on each line of the input file
             postfix_expression = preToPostfix(line)
 
-            # Write the postfix expression to the output file as prefix_expression = postfix_expression
+            # Write the postfix expression to the output file as prefix_expression = postfix_expression and skip a line
             output_file_obj.write('The output of ' + line + ' = ' + postfix_expression + '\n' + '\n')
